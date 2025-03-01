8e1f7881f16b668bda61a2db2e0d4b0f407948e6
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-03-02 00:54:22 +0530

    Added Param

--- messaging_app/src/main/java/org/ajmera/messaging_app/controller/HelloController.java
+++ messaging_app/src/main/java/org/ajmera/messaging_app/controller/HelloController.java
@@ -1,9 +1,6 @@
 package org.ajmera.messaging_app.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -15,6 +12,10 @@ public class HelloController {
 
     @GetMapping("/query")
     public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }
