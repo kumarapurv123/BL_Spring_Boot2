4983b437d8d4f8f9c2802981274cf2c1cdfc9d4f
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-03-02 00:51:51 +0530

    Query Parameter

--- messaging_app/src/main/java/org/ajmera/messaging_app/controller/HelloController.java
+++ messaging_app/src/main/java/org/ajmera/messaging_app/controller/HelloController.java
@@ -2,6 +2,7 @@ package org.ajmera.messaging_app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello From Spring Boot";
+    }
+
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
