00f07cf044b34f5055d0b6a875f54364dcee2089
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-03-01 23:14:51 +0530

    UC2

--- messaging_app/src/main/java/org/ajmera/messaging_app/controller/HelloController.java
+++ messaging_app/src/main/java/org/ajmera/messaging_app/controller/HelloController.java
@@ -1,6 +1,8 @@
 package org.ajmera.messaging_app.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
@@ -8,23 +10,5 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello From Spring Boot";
-    }
-
-    @GetMapping("/query")
-    public String sayHelloQuery(@RequestParam String name) {
-        return "Hello " + name + " from BridgeLabz";
-    }
-    @GetMapping("/param/{name}")
-    public String sayHelloPath(@PathVariable String name) {
-        return "Hello " + name + " from BridgeLabz";
-    }
-
-    @PostMapping("/post")
-    public String sayHelloPost(@RequestBody UserDTO user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
-    }
-    @PutMapping("/put/{firstName}")
-    public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
-        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
